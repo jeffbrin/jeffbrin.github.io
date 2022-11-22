49082bfe3143d155a1119fa84048bd391d3581fc
JeffBrin <bringolfj@hotmail.com>
2022-11-21 19:36:42 -0500

    Pancake button fully working

--- src/hamburger-menu.js
+++ src/hamburger-menu.js
@@ -1,6 +1,36 @@
-
-let angle = 0;
 const hamburgerButton = $("#pancake-menu-button");
-const firstChildStyle = window.getComputedStyle(hamburgerButton.children()[0]);
-hamburgerButton.children()[0].style.transform = "rotate(90deg) translateY(-100%);";
-console.log(hamburgerButton.children()[0].style)
+let navShown = false;
+const nav = $("nav")[0]
+hamburgerButton[0].addEventListener('click', e => {
+    if(navShown){
+        nav.style.top = "-110vh";
+    }
+    else{
+        nav.style.top = 0;
+    }
+
+    
+    changeHamburgerMenuAppearance(navShown);
+    navShown = !navShown;
+    
+});
+
+function changeHamburgerMenuAppearance(toX){
+    const hamburgerPatties = $(".css-menu-bar")
+    if(toX){
+        hamburgerPatties[0].style.transform = 'rotate(0)';
+        hamburgerPatties[2].style.transform = 'rotate(0)';
+        hamburgerPatties[0].style.top = "0px";
+        hamburgerPatties[2].style.top = "0px";
+
+        hamburgerPatties[1].style.background = "white";
+
+    }
+    else{
+        hamburgerPatties[0].style.transform = 'rotate(45deg)';
+        hamburgerPatties[2].style.transform = 'rotate(-45deg)';
+        hamburgerPatties[0].style.top = "7px";
+        hamburgerPatties[2].style.top = "-13px";
+        hamburgerPatties[1].style.background = "transparent"
+    }
+}
